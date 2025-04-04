5f86916f89480bc4739675d1632b0b58db57dc67
jjerxawp <hd.long501@gmail.com>
2025-04-04 15:45:43 +0700

    Initial commit with CMake setup and source files

---
+++ include/library.h
@@ -0,0 +1,13 @@
+// library.h
+#ifndef LIBRARY_H
+#define LIBRARY_H
+
+// Declare menu functions
+void showMenu();
+// void addBook();
+// void viewBooks();
+// void issueBook();
+// void returnBook();
+// void exitProgram();
+
+#endif  // LIBRARY_H
---
+++ src/library.cpp
@@ -0,0 +1,13 @@
+// library.cpp
+#include <iostream>
+#include "library.h"    // Include the library header file
+// #include "books.h"      // Include the header file for book-related functions
+
+using namespace std;
+
+void showMenu() {
+  cout << "Chào mừng đến với thư viện" << endl;
+
+
+  
+}
